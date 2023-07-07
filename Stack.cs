c04f4b7744f4f6a0c840092b8f13e8a4a22e31d7
Javier Belmonte <43077436+javierb07@users.noreply.github.com>
2023-07-07 17:02:53 -0300

    Create Stack.cs

---
+++ Stack.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+// Normally, the class that we want to test would exist in a different namespace
+namespace VendorsAPI.UnitTests {
+  public class Stack<T> {
+    private readonly List<T> _list = new List<T>();
+
+    public int Count => _list.Count;
+
+    public void Push(T obj) {
+      if (obj == null)
+        throw new ArgumentNullException();
+
+      _list.Add(obj);
+    }
+
+    public T Pop() {
+      if (_list.Count == 0)
+        throw new InvalidOperationException();
+
+      var result = _list[_list.Count - 1];
+      _list.RemoveAt(_list.Count - 1);
+
+      return result;
+    }
+
+    public T Peek() {
+      if (_list.Count == 0)
+        throw new InvalidOperationException();
+
+      return _list[_list.Count - 1];
+    }
+
+    public bool Contains(T value) {
+      if (_list.Count == 0)
+        throw new InvalidOperationException();
+      return _list.Contains(value);
+    }
+  }
+}
